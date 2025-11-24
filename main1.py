32d6c6c747051d9e688e760ffcc3d39a5b982deb
Sanket <gadilkarsnket45@gmail.com>
2025-11-24 12:20:59 +0530

    final commit

--- main1.py
+++ main1.py
@@ -109,3 +109,19 @@ def update(patient_id: str,PatientUpdate:PatientUpdate):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message':'patient updated'})
+
+@app.delete('/delete/{patient_id}')
+def delpat (patient_id: str):
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient deleted'})
+
+
+
